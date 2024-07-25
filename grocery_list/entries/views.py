9239ed164e03b752b0827895680834898c19dd6d
surfaceofbacon <rcindustries17@gmail.com>
2024-07-24 17:04:04 -0700

    added grocery form

--- grocery_list/entries/views.py
+++ grocery_list/entries/views.py
@@ -4,10 +4,22 @@ from .models import GroceryEntry
 
 
 def get_all_grocery_entries(request):
-    data = GroceryEntry.objects.all()
-    amount = GroceryEntry.objects.all().count()
+    if request.method == 'POST':
+        obj = GroceryEntry()
+        obj.name = request.POST.get('name')
+        obj.quantity = request.POST.get('quantity')
+        if obj.quantity == "":
+            obj.quantity = None
+        obj.importance_color = request.POST.get('importance_color')
+        obj.unit = request.POST.get('unit')
+        obj.brand = request.POST.get('brand')
+        obj.store = request.POST.get('store')
+        obj.save()
+
+    list_data = GroceryEntry.objects.all()
+    amount = GroceryEntry.objects.count()
     context = {
-        'groceries_list': data,
+        'groceries_list': list_data,
         'amount_in_list': amount
     }
     return render(request, 'entries/list.html', context)
