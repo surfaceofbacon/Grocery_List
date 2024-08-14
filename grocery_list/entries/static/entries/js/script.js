2de08773fe0de00b37f83ef3a1e67a00f17555a4
surfaceofbacon <rcindustries17@gmail.com>
2024-08-14 16:15:45 -0700

    fixed priority selector again^5

--- grocery_list/entries/static/entries/js/script.js
+++ grocery_list/entries/static/entries/js/script.js
@@ -3,16 +3,12 @@ const filterColorSelectEl = document.querySelector('#color-filter')
 function filterListItems() {
     const selectedOption = filterColorSelectEl.value
     const groceryEntries = document.querySelectorAll('#groceries-list li')
-    function hideIfNotColor(e) {
-        e.hidden = !e.querySelector(`div[data-color=${selectedOption}]`)
-    }
-    function showItems(j) {
-        j.hidden = false
-    }
     if (selectedOption !== "No Filter") groceryEntries.forEach(
         (e) => e.hidden = !e.querySelector(`div[data-color=${selectedOption}]`)
     )
-    else groceryEntries.forEach(showItems)
+    else groceryEntries.forEach(
+        (j) => j.hidden = false
+    )
 }
 
 filterColorSelectEl.addEventListener('change', filterListItems)
